5ded52e13dde146f41fb8cfaa5e61603b6c64835
sangdoll <pricelees@naver.com>
2023-10-05 11:23:58 +0900

    feat: Main 클래스

--- src/main/java/horseracing/Application.java
+++ src/main/java/horseracing/Application.java
@@ -1,7 +1,10 @@
 package horseracing;
 
+import horseracing.controller.HorseRacing;
+
 public class Application {
 	public static void main(String[] args) {
-
+		HorseRacing horseRacing = new HorseRacing();
+		horseRacing.startGame();
 	}
 }
