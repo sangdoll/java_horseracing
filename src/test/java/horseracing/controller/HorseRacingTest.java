dac21719b063bd84cfd2bc18ecb2d132d7342c95
sangdoll <pricelees@naver.com>
2023-10-06 12:03:30 +0900

    comment: HorseRacingTest 클래스의 주석 추가

--- src/test/java/horseracing/controller/HorseRacingTest.java
+++ src/test/java/horseracing/controller/HorseRacingTest.java
@@ -30,22 +30,28 @@ class HorseRacingTest {
 
 	/**
 	 * 각 베팅 방식에 대한 테스트를 완료하였으므로, 단승식 하나에 대해서만 게임이 정상적으로 진행되는지 확인합니다.
+	 * 랜덤 넘버를 제공하는 getNumberForRace를 재정의하여 테스트합니다.
 	 */
 	@DisplayName("게임이 정상적으로 작동하고, 종료되는지 확인합니다.")
 	@Test
 	void startGame() {
-		// 입력에 넣을 파이팅대디가 계속 우승하도록 설정
+		// 입력에 넣을 파이팅대디가 계속 우승하도록 설정합니다.
 		HorseRacing horseRacing = new HorseRacing() {
 			@Override
 			protected List<Integer> getNumberForRace() {
-				return List.of(0,0,5,0,0,0,0,0,0,0);
+				return List.of(0, 0, 5, 0, 0, 0, 0, 0, 0, 0);
 			}
 		};
-
 		horseRacing.startGame();
+
+		// 입력의 파이팅대디가 우승했을 때의 예상 상금입니다.
+		// 첫 경기에서 올인을 하므로, 첫 경기에서 이겼을 때의 잔고는 경기 결과를 통해 얻어낸 상금과 동일합니다.
 		int reward = (int)(100000 * 6.6);
+
+		// 두 번째 경기에서 이겼을 때의 예상 잔고입니다.
 		int finalBalance = (int)(100000 * 6.6) + (reward - 100000);
 
+		// 입력에 따라 정확하게 메시지를 출력하는지 확인합니다.
 		assertThat(out.toString()).contains(
 			("승리하였습니다! 획득 상금 : " + reward),
 			("상돌이님의 현재 잔고는 " + reward + "원 " + "입니다."),
