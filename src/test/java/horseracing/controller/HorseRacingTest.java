580845bb5816ff13ffe66551db34f5839ff95b0e
sangdoll <pricelees@naver.com>
2023-10-07 15:31:36 +0900

    refactor: HorseRacing의 테스트 클래스 리팩터링
    < description >
    - 테스트 조건을 바꾸고, 숫자는 가급적 변수로 선언하였음

--- src/test/java/horseracing/controller/HorseRacingTest.java
+++ src/test/java/horseracing/controller/HorseRacingTest.java
@@ -1,7 +1,6 @@
 package horseracing.controller;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -20,7 +19,8 @@ class HorseRacingTest {
 
 	@BeforeEach
 	void setUp() {
-		String input = "상돌이\n100000\n단승식\n파이팅대디\n100000\nY\n단승식\n파이팅대디\n100000\nN";
+		// 단승식으로 두 번 50000원씩 파이팅대디에게 베팅하도록 설정
+		String input = "상돌이\n100000\n단승식\n파이팅대디\n50000\nY\n단승식\n파이팅대디\n50000\nN";
 		in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
 
@@ -35,6 +35,12 @@ class HorseRacingTest {
 	@DisplayName("게임이 정상적으로 작동하고, 종료되는지 확인합니다.")
 	@Test
 	void startGame() {
+		// 단승식에 대한 배당률
+		final double odd = 6.6;
+
+		final int balanceBeforeGame = 100000;
+		final int betAmount = 50000;
+
 		// 입력에 넣을 파이팅대디가 계속 우승하도록 설정합니다.
 		HorseRacing horseRacing = new HorseRacing() {
 			@Override
@@ -45,18 +51,22 @@ class HorseRacingTest {
 		horseRacing.startGame();
 
 		// 입력의 파이팅대디가 우승했을 때의 예상 상금입니다.
-		// 첫 경기에서 올인을 하므로, 첫 경기에서 이겼을 때의 잔고는 경기 결과를 통해 얻어낸 상금과 동일합니다.
-		int reward = (int)(100000 * 6.6);
+		int reward = (int)(betAmount * odd);
+
+		// 첫 경기를 우승했을 때의 예상 잔고입니다.
+		int balanceAfterFirstGame = (balanceBeforeGame - betAmount) + reward;
 
 		// 두 번째 경기에서 이겼을 때의 예상 잔고입니다.
-		int finalBalance = (int)(100000 * 6.6) + (reward - 100000);
+		int balanceAfterSecondGame = (balanceAfterFirstGame - betAmount) + reward;
 
 		// 입력에 따라 정확하게 메시지를 출력하는지 확인합니다.
 		assertThat(out.toString()).contains(
+			"경마 게임에 오신것을 환영합니다",
 			("승리하였습니다! 획득 상금 : " + reward),
-			("상돌이님의 현재 잔고는 " + reward + "원 " + "입니다."),
+			("상돌이님의 현재 잔고는 " + balanceAfterFirstGame + "원 입니다."),
+			"다시 시도하려면 Y, 종료하려면 N를 입력해주세요",
 			("승리하였습니다! 획득 상금 : " + reward),
-			("상돌이님의 현재 잔고는 " + finalBalance + "원 " + "입니다."),
+			("상돌이님의 현재 잔고는 " + balanceAfterSecondGame + "원 입니다."),
 			"감사합니다. 안녕히 가세요.");
 	}
 }
