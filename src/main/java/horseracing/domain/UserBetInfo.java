ad209daa1bb80a33513018b092bfe060affc9f62
sangdoll <pricelees@naver.com>
2023-10-07 15:26:38 +0900

    refactor: userBetInfo 클래스 예외 조건 추가
    < description >
    - 유저의 잔고를 받아, 잔고보다 베팅 금액이 많으면 예외를 발생시키도록 설정

--- src/main/java/horseracing/domain/UserBetInfo.java
+++ src/main/java/horseracing/domain/UserBetInfo.java
@@ -9,20 +9,23 @@ public class UserBetInfo {
 	private final List<Horse> userPicks;
 	private final int betAmount;
 
-	private UserBetInfo(List<Horse> userPicks, int betAmount) {
+	private UserBetInfo(List<Horse> userPicks, int betAmount, int userBalance) {
 		if (userPicks.isEmpty()) {
 			throw new IllegalArgumentException("[ERROR] 적어도 한 마리의 말은 선택해야합니다.");
 		}
 		if (betAmount < 1000) {
 			throw new IllegalArgumentException("[ERROR] 적어도 천 원 이상 베팅해야합니다.");
 		}
+		if (betAmount > userBalance) {
+			throw new IllegalArgumentException("[ERROR] 잔고를 초과하는 금액을 베팅할 수 없습니다.");
+		}
 		this.userPicks = userPicks;
 		this.betAmount = betAmount;
 	}
 
-	public static UserBetInfo getUserBetInfo(String pick, int betAmount) {
+	public static UserBetInfo getUserBetInfo(String pick, int betAmount, int userBalance) {
 		List<Horse> userPicks = NamesToHorseListParser.parse(pick);
-		return new UserBetInfo(userPicks, betAmount);
+		return new UserBetInfo(userPicks, betAmount, userBalance);
 	}
 
 	public int getBetAmount() {
