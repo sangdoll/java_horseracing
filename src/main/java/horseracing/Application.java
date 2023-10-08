2a25a84a8e64829ac50551bb128bf8353fea43fd
sangdoll <pricelees@naver.com>
2023-10-09 03:51:21 +0900

    refactor: Application 클래스 수정
    < description >
    - 바뀐 HorseRacing 클래스에 기반하여, View를 직접 지정하도록 설정함

--- src/main/java/horseracing/Application.java
+++ src/main/java/horseracing/Application.java
@@ -1,10 +1,12 @@
 package horseracing;
 
 import horseracing.controller.HorseRacing;
+import horseracing.view.InputView;
+import horseracing.view.OutputView;
 
 public class Application {
 	public static void main(String[] args) {
-		HorseRacing horseRacing = new HorseRacing();
+		HorseRacing horseRacing = new HorseRacing(new InputView(), new OutputView());
 		horseRacing.startGame();
 	}
 }
