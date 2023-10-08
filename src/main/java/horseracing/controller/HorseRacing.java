b8551253f762cc8fb9f100e66d4726b429fb507e
sangdoll <pricelees@naver.com>
2023-10-09 03:52:12 +0900

    refactor: HorseRacing 클래스 리팩터링
    < description >
    - 유저의 잔고가 1000원 이상인지를 처음에만 확인하도록 설정
    - View의 메서드를 static으로 쓰는 것이 아닌, 직접 필드로 지정하도록 설정

--- src/main/java/horseracing/controller/HorseRacing.java
+++ src/main/java/horseracing/controller/HorseRacing.java
@@ -17,16 +17,20 @@ public class HorseRacing {
 		+ "참좋은친구,스마트전사,파워에치드,한강에이스,강철드래곤";
 	private static final int TOTAL_HORSES = 10;
 	private static final int MOVE_PER_GAME = 20;
+	private InputView inputView;
+	private OutputView outputView;
 	private GameStatus gameStatus;
 	private RaceHorses raceHorses;
 	private User user;
 
-	public HorseRacing() {
+	public HorseRacing(InputView inputView, OutputView outputView) {
+		this.inputView = inputView;
+		this.outputView = outputView;
 		this.raceHorses = RaceHorses.getFromNames(HORSE_LIST);
 	}
 
 	public void startGame() {
-		OutputView.printWelcomeMessage();
+		outputView.printWelcomeMessage();
 		receiveUserInfo();
 		gameOn();
 
@@ -42,20 +46,23 @@ public class HorseRacing {
 	}
 
 	private void receiveUserInfo() {
-		String name = InputView.getUserName();
-		int balance = InputView.getBalance();
+		String name = inputView.getUserName();
+		int balance = inputView.getBalance();
+		if (balance < 1000) {
+			throw new IllegalArgumentException("[ERROR] 잔고는 1000원 이상부터 가능합니다");
+		}
 		user = new User(name, balance);
 	}
 
 	private void play() {
 		while (GameStatus.isOn(gameStatus)) {
 			// 게임 방식을 정합니다.
-			String strategy = InputView.getStrategy();
+			String strategy = inputView.getStrategy();
 			final BettingStrategy bettingStrategy = BettingStrategy.from(strategy);
 
 			// 말 리스트와, 베팅 금액을 받아 베팅 정보를 설정합니다.
-			final String picks = InputView.getUserChoicesByStrategy(HORSE_LIST, strategy);
-			final int betAmount = InputView.getBetAmount();
+			final String picks = inputView.getUserChoicesByStrategy(HORSE_LIST, strategy);
+			final int betAmount = inputView.getBetAmount();
 			final UserBetInfo betInfo = UserBetInfo.getUserBetInfo(picks, betAmount, user.getBalance());
 
 			// 경주를 시작한 뒤, 경주 결과를 저장합니다.
@@ -63,8 +70,8 @@ public class HorseRacing {
 
 			// 경주 결과와, 유저의 베팅 정보를 바탕으로 유저의 잔고를 업데이트한뒤 출력합니다.
 			user = bettingStrategy.updateUserByBetStrategy(user, betInfo, result);
-			OutputView.printMessage(bettingStrategy.getResultMessage(betInfo, result));
-			OutputView.printUserInfo(user.getUserName(), user.getBalance());
+			outputView.printMessage(bettingStrategy.getResultMessage(betInfo, result));
+			outputView.printUserInfo(user.getUserName(), user.getBalance());
 
 			// 유저의 잔고가 1000원 이하이면 바로 종료. 아니면 재시작 여부를 묻습니다.
 			restartOrExit();
@@ -78,15 +85,15 @@ public class HorseRacing {
 	private RaceResult raceAndMakeResult() {
 		for (int i = 0; i < MOVE_PER_GAME; i++) {
 			raceHorses.moveAllHorses(getNumberForRace());
-			OutputView.printMessage(raceHorses.getPrintResult());
+			outputView.printMessage(raceHorses.getPrintResult());
 		}
 		return RaceResult.from(raceHorses.getRankResult());
 	}
 
 	private void restartOrExit() {
 		if (user.getBalance() < 1000) {
-			OutputView.printNotEnoughBalanceMessage();
-			OutputView.printEndMessage();
+			outputView.printNotEnoughBalanceMessage();
+			outputView.printEndMessage();
 			gameOff();
 			return;
 		}
@@ -94,13 +101,13 @@ public class HorseRacing {
 	}
 
 	private void decideIfRestart() {
-		String restartMessage = InputView.getRestartMessage();
+		String restartMessage = inputView.getRestartMessage();
 		if (restartMessage.equals("Y")) {
 			raceHorses = RaceHorses.getFromNames(HORSE_LIST);
 			return;
 		}
 		if (restartMessage.equals("N")) {
-			OutputView.printEndMessage();
+			outputView.printEndMessage();
 			gameOff();
 			return;
 		}
