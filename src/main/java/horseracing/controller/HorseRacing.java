f7145e540cb55062ed9a19661cb8906ce960f365
sangdoll <pricelees@naver.com>
2023-10-10 16:35:11 +0900

    refactor: 수정된 내용을 기반으로 한 HorseRacing 클래스 수정
    < description >
    - getter를 이용하여 유저의 잔고가 1000원 미만인지 체크하는 부분을 수정
    - BetProcessor 정의로 인한 play 메서드 분리

--- src/main/java/horseracing/controller/HorseRacing.java
+++ src/main/java/horseracing/controller/HorseRacing.java
@@ -5,9 +5,8 @@ import java.util.List;
 import horseracing.domain.GameStatus;
 import horseracing.domain.RaceResult;
 import horseracing.domain.User;
-import horseracing.domain.UserBetInfo;
+import horseracing.domain.BetProcessor;
 import horseracing.domain.horse.RaceHorses;
-import horseracing.domain.strategy.BettingStrategy;
 import horseracing.domain.util.RandomNumbersList;
 import horseracing.view.InputView;
 import horseracing.view.OutputView;
@@ -17,8 +16,8 @@ public class HorseRacing {
 		+ "참좋은친구,스마트전사,파워에치드,한강에이스,강철드래곤";
 	private static final int TOTAL_HORSES = 10;
 	private static final int MOVE_PER_GAME = 20;
-	private InputView inputView;
-	private OutputView outputView;
+	private final InputView inputView;
+	private final OutputView outputView;
 	private GameStatus gameStatus;
 	private RaceHorses raceHorses;
 	private User user;
@@ -56,21 +55,17 @@ public class HorseRacing {
 
 	private void play() {
 		while (GameStatus.isOn(gameStatus)) {
-			// 게임 방식을 정합니다.
-			String strategy = inputView.getStrategy();
-			final BettingStrategy bettingStrategy = BettingStrategy.from(strategy);
-
-			// 말 리스트와, 베팅 금액을 받아 베팅 정보를 설정합니다.
-			final String picks = inputView.getUserChoicesByStrategy(HORSE_LIST, strategy);
-			final int betAmount = inputView.getBetAmount();
-			final UserBetInfo betInfo = UserBetInfo.getUserBetInfo(picks, betAmount, user.getBalance());
+			// 베팅 정보를 입력받아 BetProcessor를 설정합니다.
+			BetProcessor betProcessor = receiveBetInfo();
 
 			// 경주를 시작한 뒤, 경주 결과를 저장합니다.
 			RaceResult result = raceAndMakeResult();
 
-			// 경주 결과와, 유저의 베팅 정보를 바탕으로 유저의 잔고를 업데이트한뒤 출력합니다.
-			user = bettingStrategy.updateUserByBetStrategy(user, betInfo, result);
-			outputView.printMessage(bettingStrategy.getResultMessage(betInfo, result));
+			// 경주 결과와, 유저의 베팅 정보를 바탕으로 유저의 잔고를 업데이트합니다.
+			user = betProcessor.updateUserWithResult(user, result);
+
+			// 베팅 결과와, 유저의 현재 잔고를 출력합니다.
+			outputView.printMessage(betProcessor.getResultMessage(result));
 			outputView.printUserInfo(user.getUserName(), user.getBalance());
 
 			// 유저의 잔고가 1000원 이하이면 바로 종료. 아니면 재시작 여부를 묻습니다.
@@ -78,6 +73,21 @@ public class HorseRacing {
 		}
 	}
 
+	private BetProcessor receiveBetInfo() {
+		String strategy = inputView.getStrategy();
+
+		// 말 리스트와, 베팅 금액을 받아 베팅 정보를 설정합니다.
+		final String picks = inputView.getUserChoicesByStrategy(HORSE_LIST, strategy);
+		final int betAmount = inputView.getBetAmount();
+
+		if (betAmount > user.getBalance()) {
+			throw new IllegalArgumentException("[ERROR] 잔고를 초과하는 금액을 베팅할 수 없습니다.");
+		}
+
+		return BetProcessor.of(strategy, picks, betAmount);
+	}
+
+	// 테스트를 위해 랜덤 넘버를 받아오는 메서드를 별도로 설정하였습니다.
 	protected List<Integer> getNumberForRace() {
 		return RandomNumbersList.getRandomNumbers(TOTAL_HORSES);
 	}
@@ -91,7 +101,7 @@ public class HorseRacing {
 	}
 
 	private void restartOrExit() {
-		if (user.getBalance() < 1000) {
+		if (user.isBalanceUnderThousand()) {
 			outputView.printNotEnoughBalanceMessage();
 			outputView.printEndMessage();
 			gameOff();
@@ -102,6 +112,7 @@ public class HorseRacing {
 
 	private void decideIfRestart() {
 		String restartMessage = inputView.getRestartMessage();
+
 		if (restartMessage.equals("Y")) {
 			raceHorses = RaceHorses.getFromNames(HORSE_LIST);
 			return;
@@ -111,6 +122,7 @@ public class HorseRacing {
 			gameOff();
 			return;
 		}
+
 		throw new IllegalArgumentException("[ERROR] Y / N 만 입력 가능합니다.");
 	}
 }
